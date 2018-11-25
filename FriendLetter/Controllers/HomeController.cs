244521ab74402b1a5cf5ec64afbba44dcab7e606
James Cho <hwisoocho@gmail.com>
2018-11-25 14:17:41 -0800

    route decorators

--- FriendLetter/Controllers/HomeController.cs
+++ FriendLetter/Controllers/HomeController.cs
@@ -4,8 +4,10 @@ namespace FriendLetter.Controllers
 {
   public class HomeController : Controller
   {
+    [Route("/hello")]
     public string Hello() { return "Hello friend!"; }
 
+    [Route("/goodbye")]
     public string Goodbye() { return "Goodbye friend."; }
   }
 }
